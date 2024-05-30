f6090dc44d553575ca2d68e1f98ad99e76e34f7b
Luitarr <tarallo.luigi@yahoo.co.uk>
2024-05-30 15:55:25 +0100

    kata 5

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -108,4 +108,30 @@ describe('GET /api/articles/:article_id',()=>{
        })
     })
 
+    describe('GET /api/articles',()=>{
+        test ('200:responds with an articles array of article object sorted by date(created_at) in descending order', ()=>{
+            return request(app)
+            .get('/api/articles')
+            .expect(200)
+            .then(({body})=>{
+                console.log(body)
+                const {articles}=body;
+                expect(articles).toBeSortedBy('created_at',{descending: true})
+                expect(articles).toHaveLength(13);
+                articles.forEach((article)=>{
+                    expect(article).toMatchObject({
+                        author:expect.any(String),
+                        title: expect.any(String),
+                        article_id: expect.any(Number),
+                        topic: expect.any(String),
+                        created_at: expect.any(String),
+                        votes: expect.any(Number),
+                        article_img_url: expect.any(String),
+                        comment_count: expect.any(Number)
+                    })
+                })
+            })
+        })
+    })
+
 
